d2d0c7ca5b5ae00593681f7cb5771b9726fa7ae8
vitorceolin-hotmart <vceolinn@gmail.com>
2021-05-07 15:58:09 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/springbootapirest/SpringBootApirestApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.springbootapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/alura/springbootapirest/SpringBootApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springbootapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
